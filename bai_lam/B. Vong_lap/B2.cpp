f51869a9cbb0519fbb959b18b9e6c72d5fc4c441
Anroiy123 <hunganroiy123@gmail.com>
2023-06-20 17:25:32 +0700

    add

--- bai_lam/B. Vong_lap/B2.cpp
+++ bai_lam/B. Vong_lap/B2.cpp
@@ -1,24 +1,20 @@
 #include <iostream>//Tong chu so
 using namespace std;
-
 int main()
 {
-    int n;cout<<"Nhap N: ";cin>>n;
-    while(n>1e9||n<0)
-    {
-        cout<<"Nhap lai N: ";cin>>n;
-    }
+    int n;
+    cout<<"Nhap N: ";cin>>n;
+    while(n>1e9||n<0){cout<<"Nhap lai N: ";cin>>n;}
+    //tong chu so
     int KQ = n;
     int sum;
-
-    while (n > 0) 
+    while (n != 0) 
     {
-        int digit = n % 10;
-        sum += digit;
+        int so = n % 10;
+        sum += so;
         n /= 10;
     }
-
-    cout << "Tong cac chu so cua " << KQ << " la " << sum << endl;   
+    cout << "Tong cac chu so cua " << KQ << " la : " << sum << endl;   
     system("pause");
     return 0;
 }
